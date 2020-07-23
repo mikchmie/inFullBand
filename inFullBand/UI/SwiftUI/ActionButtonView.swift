c3e138ac1f26149ea2751d49e459d9bfdb6aec76
Mikołaj Chmielewski <mikchmie@gmail.com>
2020-07-23 17:45:40 +0200

    Refactor MainView

--- inFullBand/UI/SwiftUI/ActionButtonView.swift
+++ inFullBand/UI/SwiftUI/ActionButtonView.swift
@@ -32,7 +32,7 @@ struct ActionButtonView: View {
     }
 }
 
-struct SUActionButton_Previews: PreviewProvider {
+struct ActionButtonView_Previews: PreviewProvider {
     static var previews: some View {
         ActionButtonView(emoji: "🌎", title: "Discover")
             .previewLayout(.fixed(width: 100, height: 70))
--- inFullBand/UI/SwiftUI/MainView.swift
+++ inFullBand/UI/SwiftUI/MainView.swift
@@ -18,49 +18,60 @@ struct MainView: View {
     var body: some View {
         NavigationView {
             VStack(spacing: 0.0) {
-                ScrollViewReader { scrollViewProxy in
-                    List(controller.logHistory, id: \.id) { logEntry in
-                        if logEntry.isSpecial {
-                            Button(action: {
-                                self.controller.logEntrySelectedAction(logEntry: logEntry)
-                            }) {
-                                LogListCell(logEntry: logEntry)
-                            }
-                        } else {
-                            LogListCell(logEntry: logEntry)
-                        }
-                    }
-                    .onChange(of: controller.logHistory) { _ in
-                        if let lastRowID = controller.logHistory.last?.id {
-                            scrollViewProxy.scrollTo(lastRowID)
-                        }
-                    }
-                }
-                VStack(spacing: 0.0) {
-                    VStack(spacing: 6.0) {
-                        ForEach(self.makeButtonDescriptors(), id: \.self) { sectionItems in
-                                HStack(spacing: 6.0) {
-                                    ForEach(sectionItems, id: \.self) { item in
-                                        Button(action: item.action) {
-                                            ActionButtonView(emoji: item.emoji, title: item.title)
-                                        }
-                                    }
-                                }
-                        }
-                    }
-                    .padding(6.0)
-                    .background(Color.black)
-                    Rectangle()
-                        .foregroundColor(.black)
-                        .frame(height: UIApplication.shared.windows.first?.safeAreaInsets.bottom)
-                }
+                list
+                controlPad
             }
             .navigationBarTitle(Text("Activity"))
             .edgesIgnoringSafeArea(.bottom)
         }
     }
     
-    private func makeButtonDescriptors() -> [[ButtonDescriptor]] {
+    private var list: some View {
+        ScrollViewReader { scrollViewProxy in
+            List(controller.logHistory) { logEntry in
+                if logEntry.isSpecial {
+                    Button(action: {
+                        controller.logEntrySelectedAction(logEntry: logEntry)
+                    }) {
+                        LogListCell(logEntry: logEntry)
+                    }
+                } else {
+                    LogListCell(logEntry: logEntry)
+                }
+            }
+            .listStyle(PlainListStyle())
+            .onChange(of: controller.logHistory) { _ in
+                if let lastRowID = controller.logHistory.last?.id {
+                    withAnimation {
+                        scrollViewProxy.scrollTo(lastRowID)
+                    }
+                }
+            }
+        }
+    }
+    
+    private var controlPad: some View {
+        VStack(spacing: 0.0) {
+            VStack(spacing: 6.0) {
+                ForEach(buttonDescriptors, id: \.self) { sectionItems in
+                    HStack(spacing: 6.0) {
+                        ForEach(sectionItems, id: \.self) { item in
+                            Button(action: item.action) {
+                                ActionButtonView(emoji: item.emoji, title: item.title)
+                            }
+                        }
+                    }
+                }
+            }
+            .padding(6.0)
+            .background(Color.black)
+            Rectangle()
+                .foregroundColor(.black)
+                .frame(height: UIApplication.shared.windows.first?.safeAreaInsets.bottom)
+        }
+    }
+    
+    private var buttonDescriptors: [[ButtonDescriptor]] {
         [[ButtonDescriptor(emoji: "🌎", title: "Discover", action: controller.discoverButtonAction),
           ButtonDescriptor(emoji: "❌", title: "Disconnect", action: controller.disconnectButtonAction),
           ButtonDescriptor(emoji: "📶", title: "Update stats", action: controller.updateButtonAction)],
